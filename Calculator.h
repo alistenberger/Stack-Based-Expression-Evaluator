43bcf7eb5e12bee278f7076451714f4cf1254a28
alistenberger <alistenb@iu.edu>
2021-04-03 18:40:25 -0400

    Began adding calculator

---
+++ Calculator.h
@@ -0,0 +1,56 @@
+#ifndef _CALCULATOR_H_
+#define _CALCULATOR_H_
+
+#include "Expr_Command_Factory.h"
+#include "Expr_Command.h"
+#include "Stack_Expr_Command_Factory.h"
+#include "Number_Command.h"
+#include "Add_Command.h"
+#include "Subtract_Command.h"
+#include "Multiply_Command.h"
+#include "Divide_Command.h"
+#include "Modulus_Command.h"
+#include "Stack.h"
+#include "Queue.h"
+#include "Array.h"
+#include "Base_Array.h"
+#include "Fixed_Array.h"
+#include <iostream>
+#include <sstream>
+
+/**
+ * @class Calculator
+ *
+ * A calculator which utilizes a Stack-Based Evaluator to calculate a given expression
+ */
+
+class Calculator
+{
+  /// Constructor
+  Calculator (void);
+  
+  /// Destructor
+  ~Calculator (void);
+
+  /**
+   * Parses an infix expression to postfix format
+   * 
+   * @param[in]     infix     the infix format delineated by spaces (or QUIT)
+   * @param[in]     factory   the expression command factory responsible for generation of expression commands
+   * @param[in]     postfix   the array which will store the postfix expression
+   * 
+   * @retval        returns true on successful completion
+   */
+  bool infix_to_postfix (const std::string & infix, Expr_Command_Factory & factory, Array <Expr_Command *> & postfix);
+  
+  /**
+   * Evaluates a postfix expression utilizing a stack
+   * 
+   * @param[in]     postfix   an array class object containing the postfix expression
+   * 
+   * @retval        returns true on successful completion
+   */
+  bool evaluate_postfix (Array <Expr_Command *> & postfix);
+};
+
+#endif // !defined _CALCULATOR_H_
